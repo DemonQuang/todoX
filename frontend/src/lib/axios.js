8baa48942bc6cab675e550b0aedbd8078041ce39
Huy Hoang <163020090+DemonQuang@users.noreply.github.com>
2026-04-26 19:07:37 +0700

    fix bug axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const BASE_URL =
-    import.meta.env.MODE === "development"
-        ? "http://localhost:3007/api"
-        : "https://qtodox.onrender.com/api";
+// const BASE_URL =
+//     import.meta.env.MODE === "development"
+//         ? "http://localhost:3007/api"
+//         : "https://qtodox.onrender.com/api";
 
 const api = axios.create({
-    baseURL: BASE_URL,
+    baseURL: "https://qtodox.onrender.com/api"
 });
 
 export default api;
