75e6f2cbd46aa9ccb00f6aacfdf34d6a9fe45dfb
Huy Hoang <163020090+DemonQuang@users.noreply.github.com>
2026-04-26 15:35:28 +0700

    fix deploy bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,13 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import path from 'path';
+
 dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 3007;
 const __dirname = path.resolve();
 
-const app = express();
 app.use(express.json());
 
 if (process.env.NODE_ENV !== 'production') {
@@ -19,6 +20,9 @@ if (process.env.NODE_ENV !== 'production') {
     }));
 }
 
+
+
+// 🔥 serve frontend
 app.use('/api/tasks', taskRoutes);
 
 if (process.env.NODE_ENV === 'production') {
@@ -27,12 +31,8 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
-
 }
 
-
-
-app.use('/api/tasks', taskRoutes);
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
