1f869cb6e4bb1c2ecd49f2ad803a3368a5d5290a
Huy Hoang <163020090+DemonQuang@users.noreply.github.com>
2026-04-26 14:48:22 +0700

    cb deploy

--- be/src/app.js
+++ be/src/app.js
@@ -3,19 +3,33 @@ import taskRoutes from './route/taskRoute.js';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
-app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 3007;
+const __dirname = path.resolve();
 
-// Middleware
+const app = express();
 app.use(express.json());
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: "http://localhost:5173",
+        credentials: true
+    }));
+}
+
+app.use('/api/tasks', taskRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../FE/dist")));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../FE/dist/index.html"));
+    });
+
+}
+
 
 
 app.use('/api/tasks', taskRoutes);
