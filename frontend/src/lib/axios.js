8e58ed1b8ee12d1e554a542e51389f0e8208faa6
Huy Hoang <163020090+DemonQuang@users.noreply.github.com>
2026-04-26 16:34:26 +0700

    fix apixdsdsdádasda  url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const BASE_URL =
-    import.meta.env.MODE === "/api";
+    import.meta.env.MODE === "development"
+        ? "http://localhost:3007/api"
+        : "/api";
 
 const api = axios.create({
-    baseURL: "/api"
+    baseURL: BASE_URL,
 });
 
 export default api;
